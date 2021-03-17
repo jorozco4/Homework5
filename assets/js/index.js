b28b2ece23f23b77ca34149e478965bc75d0d774
Jesse Orozco <orozcojesse0@gmail.com>
2021-03-17 18:28:15 -0400

    restructured project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,2 @@
-let today = new Date();
-
-let date =
-  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-
-let time =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-let dateTime = date + " " + time;
+let today = moment().format("MMMM Do YYYY dddd, h:mm:ss a");
+document.getElementById("currentDay").innerHTML = today;
